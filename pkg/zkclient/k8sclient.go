e6dadeffec72ef0e965b21ab0542de4cddb4a8f6
rajeevrajeshuni <rajeev@rajeshuni.com>
2023-04-17 10:04:13 +0530

    Adding methods to k8sclient for working with labels.

--- pkg/zkclient/k8sclient.go
+++ pkg/zkclient/k8sclient.go
@@ -1,10 +1,22 @@
 package zkclient
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+
 	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
+
+type patchStringValue struct {
+	Op    string `json:"op"`
+	Path  string `json:"path"`
+	Value string `json:"value"`
+}
 
 var (
 	dockerConfigKey string = ".dockerconfigjson"
@@ -24,6 +36,40 @@ func GetImagePullSecrets(pod *corev1.Pod) []string {
 		secrets = append(secrets, imagePullSecret.Name)
 	}
 	return secrets
+}
+
+func LabelPod(pod *corev1.Pod, path string, value string) {
+	k8sClient := GetK8sClient().CoreV1()
+	payload := []patchStringValue{{
+		Op:    "replace",
+		Path:  path,
+		Value: value,
+	}}
+	payloadBytes, _ := json.Marshal(payload)
+	_, updateErr := k8sClient.Pods(pod.GetNamespace()).Patch(context.Background(), pod.GetName(), types.JSONPatchType, payloadBytes, metav1.PatchOptions{})
+	if updateErr == nil {
+		logMessage := fmt.Sprintf("Pod %s labeled successfully for Path %s and Value %s.", pod.GetName(), path, value)
+		fmt.Println(logMessage)
+	} else {
+		fmt.Println(updateErr)
+	}
+}
+
+func getPodsWithSelector(selector string) *corev1.PodList {
+	clientset := GetK8sClient()
+	listOptions := metav1.ListOptions{
+		LabelSelector: selector,
+	}
+	pods, _ := clientset.CoreV1().Pods("").List(context.Background(), listOptions)
+	return pods
+}
+
+func getPodsWithLabel(labelKey, labelValue string) *corev1.PodList {
+	return getPodsWithSelector(labelKey + "=" + labelValue)
+}
+
+func getPodsWithOutLabel(labelKey string) *corev1.PodList {
+	return getPodsWithSelector("!" + labelKey)
 }
 
 func GetK8sClient() *kubernetes.Clientset {
