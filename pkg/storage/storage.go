eb33b0a8aa45031ddc8c56f46f3f63034832fdbe
rajeevrajeshuni <rajeev@rajeshuni.com>
2023-04-18 11:23:56 +0530

    Added logic to store image runtime information

--- pkg/storage/storage.go
+++ pkg/storage/storage.go
@@ -1,9 +1,31 @@
 package storage
 
-func saveRuntimeForImage() {
+import (
+	"sync"
+)
 
+type ImageRuntimeStorage struct {
+	ImageRuntimeMap sync.Map
 }
 
-func getRuntimeForImage() {
+type RuntimeDetails struct {
+	runtime string
+	cmdLine string
+}
 
+func (h *ImageRuntimeStorage) saveRuntimeForImage(imageID string, runtimeDetails *RuntimeDetails) {
+	h.ImageRuntimeMap.Store(imageID, runtimeDetails)
+}
+
+func (h *ImageRuntimeStorage) getRuntimeForImage(imageID string) *RuntimeDetails {
+	value, ok := h.ImageRuntimeMap.Load(imageID)
+	if !ok {
+		return nil
+	}
+	switch y := value.(type) {
+	case *RuntimeDetails:
+		return y
+	default:
+		return nil
+	}
 }
