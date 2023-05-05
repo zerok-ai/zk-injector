28f76a36de2702b9fe9a142a5418394e352a5d22
rajeevrajeshuni <rajeev@rajeshuni.com>
2023-05-05 17:01:29 +0530

    Renamed files for consistency and removed duplicate cofig.go file

--- utils-go/redis/config/config.go
+++
@@ -1,9 +0,0 @@
-package config
-
-// Commenting here since it's a duplicate struct also present in internal/config/config.go file.
-
-// type RedisConfig struct {
-// 	Host        string `yaml:"host" env:"REDIS_HOST" env-description:"Database host"`
-// 	Port        string `yaml:"port" env:"REDIS_PORT" env-description:"Database port"`
-// 	ReadTimeout int    `yaml:"readTimeout"`
-// }
